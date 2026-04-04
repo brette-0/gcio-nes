7e4a61e88843904efca3a5652a3cd3a4d965a8c0
Brette <98058652+brette-0@users.noreply.github.com>
2026-04-04 13:00:26 +0100

    console-adaptor work

---
+++ src/main.h
@@ -0,0 +1,41 @@
+#ifndef  MAIN_H
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+extern volatile uint64_t raw;
+extern volatile uint64_t capture;
+extern volatile uint64_t flip;
+extern volatile uint64_t mask;
+
+extern volatile uint8_t  behavior;
+extern volatile uint8_t  nInupts;
+
+extern volatile uint8_t  shift;
+extern volatile uint8_t  latch;
+
+extern volatile uint8_t  task;
+extern volatile uint8_t  nTask;
+
+extern volatile uint8_t  __PD2;
+
+int main(void);
+
+inline void init_latch_interrupt(void);
+inline void console_read(void);
+inline void console_write(void);
+
+
+ISR(INT0_vect) { console_write(); }
+
+ISR(INT1_vect) { console_read(); }
+
+enum ETasks {
+    LEGACY,
+    REPORT,
+    INMASK,
+    INVERT,
+    RUMBLE,
+    LSETUP
+};
+
+#endif
